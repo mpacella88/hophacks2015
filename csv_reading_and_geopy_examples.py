42949ac47b761f5cf5417daece11ca53ba772c66
Michael Pacella <mpacella88@gmail.com>
2015-09-12 01:29:31 -0400

    adding simple examples of csv reading and longitude/latitude

---
+++ csv_reading_and_geopy_examples.py
@@ -0,0 +1,18 @@
+#csv example
+import csv
+with open('/Users/mpacella/Downloads/DOT_Towing.csv') as f:
+    reader = csv.reader(f)
+    tow_list = list(reader)
+
+#geopy example
+#grab address and city fields from tow_list and pass to geolocator
+import geopy
+from geopy.geocoders import Nominatim
+geolocator = Nominatim()
+location = geolocator.geocode(3957  BROOKLYN AVE BALTIMORE)
+location.latitude
+location.longitude
+
+
+
+
