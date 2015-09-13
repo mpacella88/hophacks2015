a24d1a4028b0762653bd023ae420bf7802b35a8d
hlessen <aggiefan13@gmail.com>
2015-09-13 02:40:47 -0400

    Added time separation

--- test-openbalt.py
+++ test-openbalt.py
@@ -6,25 +6,53 @@ import requests
 import numpy as np
 
 def data_assemble():
-    #data_311 = get_data("https://data.baltimorecity.gov/resource/9agw-sxsr.csv")           # Works
+    #data_311 = get_data("https://data.baltimorecity.gov/resource/9agw-sxsr.csv")           # Works !! Can get GPS Data !!
     #employee_data_14 = get_data("https://data.baltimorecity.gov/resource/2j28-xzd7.csv")   # Works
     crime_data = get_data("https://data.baltimorecity.gov/resource/3i3v-ibrt.csv")         # Works
-    #parking_citation = get_data("https://data.baltimorecity.gov/resource/n4ma-fj3m.csv")   # Works
-    #property_taxes = get_data("https://data.baltimorecity.gov/resource/27w9-urtv.csv")     # Works
-    vacant_building = get_data("https://data.baltimorecity.gov/resource/qqcv-ihn5.csv")    # Works
-    liquor_license = get_data("https://data.baltimorecity.gov/resource/xv8d-bwgi.csv")     # Works
-    #towing_data = get_data("https://data.baltimorecity.gov/resource/k78j-azhn.csv")        # Works
-    test_dat0 = extract_long_lat2(liquor_license)
-    test_dat = extract_long_lat1(vacant_building)
-    test_dat2 = extract_long_lat1(crime_data)
-    counts_liquor = bin_balt(-76.681240,39.373947,-0.147814,0.10631,10,10,test_dat0)
-    counts_vacant = bin_balt(-76.681240,39.373947,-0.147814,0.10631,10,10,test_dat)
-    counts_crime = bin_balt(-76.681240,39.373947,-0.147814,0.10631,10,10,test_dat2)
-    test = np.hstack((counts_liquor.reshape((100,1)),counts_vacant.reshape((100,1)),counts_crime.reshape((100,1))))
-    print
-    print test
+    #parking_citation = get_data("https://data.baltimorecity.gov/resource/n4ma-fj3m.csv")   # Works !! Can get GPS Data !!
+    #property_taxes = get_data("https://data.baltimorecity.gov/resource/27w9-urtv.csv")     # Works !! Can get GPS Data !!
+    #vacant_building = get_data("https://data.baltimorecity.gov/resource/qqcv-ihn5.csv")    # Works
+    #liquor_license = get_data("https://data.baltimorecity.gov/resource/xv8d-bwgi.csv")     # Works
+
+    # This part I will write out csv files in to be loaded as a function of time
+    coor = extract_long_lat1(crime_data)
+    date = list(crime_data['ArrestDate'])
+    write_time_data(date,coor)
+
+    #test_dat0 = extract_long_lat2(liquor_license)
+    #test_dat = extract_long_lat1(vacant_building)
+    #test_dat2 = extract_long_lat1(crime_data)
+    #counts_liquor = bin_balt(-76.681240,39.373947,-0.147814,0.10631,10,10,test_dat0)
+    #counts_vacant = bin_balt(-76.681240,39.373947,-0.147814,0.10631,10,10,test_dat)
+    #counts_crime = bin_balt(-76.681240,39.373947,-0.147814,0.10631,10,10,test_dat2)
+    #test = np.hstack((counts_liquor.reshape((100,1)),counts_vacant.reshape((100,1)),counts_crime.reshape((100,1))))
     #print
-    print
+    #print test
+    return
+
+def write_time_data(date,coor):
+    for x in range(0,len(coor)):
+        if int(date[x][3:5]) == 5:
+            outfile = open('crime7.csv','a')
+            outfile.write("%f %f\n"%(coor[x][0],coor[x][1]))
+        elif int(date[x][3:5]) == 4:
+            outfile = open('crime6.csv','a')
+            outfile.write("%f %f\n"%(coor[x][0],coor[x][1]))
+        elif int(date[x][3:5]) == 3:
+            outfile = open('crime5.csv','a')
+            outfile.write("%f %f\n"%(coor[x][0],coor[x][1]))
+        elif int(date[x][3:5]) == 2:
+            outfile = open('crime4.csv','a')
+            outfile.write("%f %f\n"%(coor[x][0],coor[x][1]))
+        elif int(date[x][3:5]) == 1:
+            outfile = open('crime3.csv','a')
+            outfile.write("%f %f\n"%(coor[x][0],coor[x][1]))
+        elif int(date[x][3:5]) == 31:
+            outfile = open('crime2.csv','a')
+            outfile.write("%f %f\n"%(coor[x][0],coor[x][1]))
+        elif int(date[x][3:5]) == 30:
+            outfile = open('crime1.csv','a')
+            outfile.write("%f %f\n"%(coor[x][0],coor[x][1]))
     return
 
 # This is a function that extracts long and lat for
@@ -36,6 +64,7 @@ def extract_long_lat1(data_set):
             test_dat.append([ float(y.strip()) for y in x[1:-1].split(',') ])
     return test_dat
 
+# This function extract GPS data from the liquor license data set
 def extract_long_lat2(data_set):
     test_dat = []
     for x in data_set['Location 1']:
